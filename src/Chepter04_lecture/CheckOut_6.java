bcb5b7aba6b5d4da24aa9180ba3a843e8496dffc
June <junetiger10@naver.com>
2024-08-22 17:42:12 +0900

    객체지향프로그래밍

--- src/Chepter04_lecture/CheckOut_6.java
+++ src/Chepter04_lecture/CheckOut_6.java
@@ -13,3 +13,4 @@ public class CheckOut_6 {
     }
 }
 
+
---
+++ src/Chepter04_lecture/checkOut_var1.java
@@ -0,0 +1,26 @@
+package Chepter04_lecture;
+
+public class checkOut_var1 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        // ver1
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k <= j; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // %10을 이용한 버젼
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k <= j; k++) {
+                System.out.print(i % 10);
+                i++;
+            }
+            System.out.println();
+        }
+    }
+}
