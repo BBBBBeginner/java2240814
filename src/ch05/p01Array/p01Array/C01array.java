654f537b05c24eb0f8cea2aeda90baa0cd78c911
June <junetiger10@naver.com>
2024-08-22 17:42:30 +0900

    객체지향프로그래밍

---
+++ src/ch05/p01Array/p01Array/C01array.java
@@ -0,0 +1,22 @@
+package ch05.p01Array.p01Array;
+
+public class C01array {
+    public static void main(String[] args) {
+        int a;
+        a = 3;
+        System.out.println("a =" + a);
+
+        a = 4;
+        System.out.println("a =" + a);
+
+        int[] b; // 하나에 한개 저장이 아닌 여러개 저장 가능
+        b = new int[5];
+        b[0] = 1;
+        b[1] = 10;
+        b[2] = 100;
+
+        // 배열 (array) : 여러 값을 가지는 자료형(자료구조)
+
+
+    }
+}
