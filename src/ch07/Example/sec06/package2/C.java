5e83cac403c404d726c2541702f93a91205d3c4d
June <junetiger10@naver.com>
2024-08-30 16:58:38 +0900

    protected

---
+++ src/ch07/Example/sec06/package2/C.java
@@ -0,0 +1,11 @@
+package ch07.Example.sec06.package2;
+
+public class C {
+
+    //메소드 선언
+    public void method() {
+//      A a = new A();    //x
+//      a.field = "value";//x
+//      a.method();       //x
+    }
+}
---
+++ src/ch07/Example/sec06/package2/D.java
@@ -0,0 +1,6 @@
+package ch07.Example.sec06.package2;
+
+public class D {
+
+
+}
