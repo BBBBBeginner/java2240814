c759a3a5700b2c69f3dfd7e24e4bcec8c32a90b6
June <junetiger10@naver.com>
2024-09-02 16:45:40 +0900

    상속 개념

---
+++ src/ch08/sec03/RemoteControlExample.java
@@ -0,0 +1,8 @@
+package ch08.sec03;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        System.out.println("리모컨 볼륨 최대 :" + RemoteControl.MAX_VOLUME);
+        System.out.println("리모컨 볼륨 최저 :" + RemoteControl.MIN_VOLUME);
+    }
+}
