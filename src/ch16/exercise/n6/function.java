7bbfbe5e813e3ae360248d6b6f02dd8dee008fbb
June <junetiger10@naver.com>
2024-09-03 16:57:36 +0900

    확인문제 / 메소드 참조

---
+++ src/ch16/exercise/n6/function.java
@@ -0,0 +1,5 @@
+package ch16.exercise.n6;
+
+interface Function {
+    double apply(double x, double y);
+}
---
+++ src/ch16/exercise/n7/Operator.java
@@ -0,0 +1,8 @@
+package ch16.exercise.n7;
+
+@FunctionalInterface
+public interface Operator {
+    public int apply(int x, int y);
+}
+
+
