e186a07a54af2df97802f536615d7354570ecb9f
June <junetiger10@naver.com>
2024-09-03 16:59:03 +0900

    파라미터

---
+++ src/ch16/lecture/p01lambda/C07Parameter.java
@@ -0,0 +1,13 @@
+package ch16.lecture.p01lambda;
+
+public class C07Parameter {
+    public static void main(String[] args) {
+        MyInterface7 k = () -> System.out.println();
+        k.print();
+    }
+}
+
+@FunctionalInterface
+interface MyInterface7 {
+    void print();
+}
