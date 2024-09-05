3869d4be92610194ff117ca7eb12ed10990d1d57
June <junetiger10@naver.com>
2024-09-05 16:49:29 +0900

    중첩된 스태틱 클래스

---
+++ src/ch09/sec03/ex1/A.java
@@ -0,0 +1,24 @@
+package ch09.sec03.ex1;
+
+public class A {
+
+    static class B {
+    }
+
+    //인스턴스 필드값으로 B 객체 대입
+    B field1 = new B();
+
+    //생성자
+    A() {
+        B b = new B();
+    }
+
+    //인스턴스 메소드
+    void method1() {
+        B b = new B();
+    }
+
+    static void method2() {
+        B b = new B();
+    }
+}
