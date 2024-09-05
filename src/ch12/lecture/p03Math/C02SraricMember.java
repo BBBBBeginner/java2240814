ec6aad0f4c539c9536db64178fa4bccec919789a
June <junetiger10@naver.com>
2024-09-05 16:53:31 +0900

    Math static methods

---
+++ src/ch12/lecture/p03Math/C02SraricMember.java
@@ -0,0 +1,19 @@
+package ch12.lecture.p03Math;
+
+// static import
+
+import static java.lang.Math.PI;
+import static java.util.Arrays.copyOf;
+
+public class C02SraricMember {
+    public static void main(String[] args) {
+        double p = PI; // static field;
+
+        int m = Math.max(300, 500); // static method;
+
+        int[] a = {5, 9, 1};
+        int[] b = copyOf(a, a.length);
+
+
+    }
+}
