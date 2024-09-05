ea44e89c8ed286fab7100ed8e871514adde99434
June <junetiger10@naver.com>
2024-09-05 16:52:59 +0900

    boxing, unboxing

---
+++ src/ch12/lecture/p02Wrapper/C02Wrapper.java
@@ -0,0 +1,14 @@
+package ch12.lecture.p02Wrapper;
+
+public class C02Wrapper {
+    Integer a = 5;
+
+    // 오래전 바꾸는 방법...
+    // 직접 언박싱
+    int b = a.intValue();
+
+
+    //현제
+    //auto unboxing
+    int c = a;
+}
