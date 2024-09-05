0340faa359149756558d5495f871e0676d94fb49
June <junetiger10@naver.com>
2024-09-05 16:48:03 +0900

    중첩클래스

---
+++ src/ch09/lecture/p02nested/C01NestedClass.java
@@ -0,0 +1,25 @@
+package ch09.lecture.p02nested;
+
+public class C01NestedClass {
+    public static void main(String[] args) {
+        MyClass1 a = new MyClass1();
+        MyClass1.Nested1 b = a.new Nested1();
+    }
+
+}
+
+class MyClass1 {
+    //class
+    class Nested1 {
+        //필드
+        //생성자
+        //메소드
+    }
+
+    //피드
+    //생성자
+    //메소드
+    void someMethod() {
+
+    }
+}
