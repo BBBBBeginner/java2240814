9f23ab21d523055c381df94b5731c9056d1d32a9
June <junetiger10@naver.com>
2024-09-06 17:04:51 +0900

    static import

--- src/ch12/lecture/p03Math/C02SraricMember.java
+++ src/ch12/lecture/p03Math/C02SraricMember.java
@@ -8,12 +8,11 @@ import static java.util.Arrays.copyOf;
 public class C02SraricMember {
     public static void main(String[] args) {
         double p = PI; // static field;
-
+//        System.out.println(p);
         int m = Math.max(300, 500); // static method;
 
         int[] a = {5, 9, 1};
         int[] b = copyOf(a, a.length);
-
 
     }
 }
