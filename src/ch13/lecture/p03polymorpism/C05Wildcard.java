00a28a63587163ce4e885f83fb881fdaa7ef1ff6
June <junetiger10@naver.com>
2024-09-06 17:05:53 +0900

    wildcard

---
+++ src/ch13/lecture/p03polymorpism/C05Wildcard.java
@@ -0,0 +1,27 @@
+package ch13.lecture.p03polymorpism;
+
+public class C05Wildcard {
+    public static void main(String[] args) {
+        Box5<String> b1 = new Box5<String>();
+
+        // 만약 되면
+//        Box5<Object> b2 = new Box5<String>();
+
+        // 다음 코드도 되야함
+//    b2.setItem(new Object());
+
+        //wildcard
+        Box5<?> b3 = new Box5<String>();
+        Box5<?> b4 = new Box5<Long>();
+
+
+    }
+}
+
+class Box5<T> {
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+}
