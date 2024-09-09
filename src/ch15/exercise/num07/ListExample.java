a76379bc6fd4d01d4a2a4c03cca3d7aa45535436
June <junetiger10@naver.com>
2024-09-09 17:53:33 +0900

    set

--- src/ch15/exercise/num07/ListExample.java
+++ src/ch15/exercise/num07/ListExample.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ListExample {
     public static void main(String[] args) {
         BoardDao dao = new BoardDao();
-        List<Board> list = dao.getBoardList();
+        List<Board> list = dao.getBoardList(); //List : 인터페이스, <board> : 클래스
         for (Board board : list) {
             System.out.println(board.getTitle() + "-" + board.getContent());
         }
