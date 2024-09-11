270a0937fb7faff414802efbafd9ff88ac530ea0
June <junetiger10@naver.com>
2024-09-11 17:01:46 +0900

    stream

---
+++ src/ch17/lecture/p01stream/C01Operation.java
@@ -0,0 +1,16 @@
+package ch17.lecture.p01stream;
+
+public class C01Operation {
+    public static void main(String[] args) {
+        //Stream 연산은 아래 3가지를 거쳐야함
+
+        // 스트림 연산(메소드)
+
+        // 1. 중간 연산/처리(intermediate operation)
+        // return : Stream(intStream DoubleStream, Longstream)
+
+        // 2. 최종 연산/처리(terminal operation)
+        // return : Stream이 아님
+
+    }
+}
