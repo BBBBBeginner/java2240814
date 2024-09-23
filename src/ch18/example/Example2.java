5804a57043e8a6c8c43daf9509461595ea253fd8
June <junetiger10@naver.com>
2024-09-23 18:04:56 +0900

    확인문제 7

---
+++ src/ch18/example/Example2.java
@@ -0,0 +1,10 @@
+package ch18.example;
+
+import java.io.IOException;
+
+public class Example2 {
+    public static void main(String[] args) throws IOException {
+        String path = "src/ch18/exercise/p7/Example.java";
+
+    }
+}
