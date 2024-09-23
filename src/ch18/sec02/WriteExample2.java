0140ed10f4a03723fc1f3d2547edc23cd89412bf
June <junetiger10@naver.com>
2024-09-23 18:15:21 +0900

    write

---
+++ src/ch18/sec02/WriteExample2.java
@@ -0,0 +1,18 @@
+package ch18.sec02;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample2 {
+    public static void main(String[] args) {
+        try (OutputStream os = new FileOutputStream("C:/Temp/test3.db")) {
+            byte[] arr = {10, 20, 30, 40, 50};
+
+            os.write(arr, 1, 3);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
