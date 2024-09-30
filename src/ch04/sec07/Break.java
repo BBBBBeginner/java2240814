a36216815bf0b5e3cacaafe50a229ae50c405e83
June <junetiger10@naver.com>
2024-09-30 17:29:44 +0900

    break

--- src/ch04/sec07/Break.java
+++ src/ch04/sec07/Break.java
@@ -1,15 +1,14 @@
-package ch04_lecture.p05_ex;
+package ch04.sec07;
 
-public class BreakExample {
+public class Break {
     public static void main(String[] args) {
         while (true) {
             int num = (int) (Math.random() * 6) + 1;
             System.out.println(num);
+
             if (num == 6) {
                 break;
             }
         }
-        System.out.println("프로그램 종료");
     }
-
 }
